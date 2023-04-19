51a67e410ade775431f929d71f486f1b6b3d3a96
Gil Zanabria <gzanabriaz28@gmail.com>
2023-04-18 21:49:32 -0500

    homecontroller

--- src/main/java/com/example/lab420200334/controller/HomeController.java
+++ src/main/java/com/example/lab420200334/controller/HomeController.java
@@ -1,4 +1,47 @@
 package com.example.lab420200334.controller;
 
+
+import com.example.lab420200334.entity.User;
+import com.example.lab420200334.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
 public class HomeController {
+
+
+    final UserRepository userRepository;
+    public HomeController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @GetMapping("/")
+    public String index(){
+        return "inicio";
+    }
+
+
+    @PostMapping("/login/inicioSesion")
+    public String validarcredencial(@RequestParam("correo") String correo, @RequestParam("constrasena") String constrasena, Model model ){
+        Optional<User> optUser = Optional.ofNullable(userRepository.encontrarUsuario(correo, constrasena));
+
+        if (optUser.isPresent()) {
+            User user = optUser.get();
+            model.addAttribute("user", user);
+            return "homePage";
+        } else {
+            return "redirect:/";
+        }
+
+
+
+
+
+        return "inicio";
+    }
+
 }
--- src/main/java/com/example/lab420200334/repository/UserRepository.java
+++ src/main/java/com/example/lab420200334/repository/UserRepository.java
@@ -3,8 +3,16 @@ package com.example.lab420200334.repository;
 
 import com.example.lab420200334.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(value = "select * from user where email = ?1 and password = ?2 ",
+            nativeQuery = true)
+    User encontrarUsuario(String email,String contrasena);
+
+
+
 }
