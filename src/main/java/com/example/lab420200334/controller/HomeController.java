ecde22277915357301695683227ea252ab332848
xTarantino28 <gzanabriaz28@gmail.com>
2023-04-23 18:06:45 -0500

    lab terminado. efe modal

--- src/main/java/com/example/lab420200334/controller/HomeController.java
+++ src/main/java/com/example/lab420200334/controller/HomeController.java
@@ -8,9 +8,8 @@ import com.example.lab420200334.repository.UserRepository;
 import com.example.lab420200334.repository.VueloRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,23 +32,59 @@ public class HomeController {
     }
 
 
-    @PostMapping("/login/inicioSesion")
-    public String validarcredencial(@RequestParam("correo") String correo, @RequestParam("contrasena") String contrasena, Model model ){
+    @GetMapping("/homePage")
+    public String homePage(Model model){
+        List listaVuelos = vueloRepository.findAll();
+        model.addAttribute("listaVuelos", listaVuelos);
+        return "homePage";
+    }
+
+
+
+    @RequestMapping(value = "/login/inicioSesion",  method = { RequestMethod.GET, RequestMethod.POST } )
+    public String validarcredencial(@RequestParam("correo") String correo, @RequestParam("contrasena") String contrasena, Model model, RedirectAttributes attr ){
         Optional<User> optUser = Optional.ofNullable(userRepository.encontrarUsuario(correo, contrasena));
 
         if (optUser.isPresent()) {
             User user = optUser.get();
-
+            //attr.addAttribute("user_iduser",user.getIduser());
+            //System.out.println(user.getIduser());
+            model.addAttribute("user",user);
             List listaVuelos = vueloRepository.findAll();
-            model.addAttribute("user", user);
             model.addAttribute("listaVuelos", listaVuelos);
             return "homePage";
+
 
         } else {
             return "redirect:/";
         }
-
-
     }
+
+
+    @RequestMapping(value ="/guardarReserva", method = { RequestMethod.GET, RequestMethod.POST })
+    public String guardarReserva(@RequestParam("vuelo_idvuelo") int vuelo_idvuelo,
+                                 @RequestParam("user_iduser") int user_iduser,
+                                 Model model,
+                                 @RequestParam("precio_total") String precio_total){
+        Double precio = Double.parseDouble(precio_total);
+        reservaRepository.guardarReserva(vuelo_idvuelo, user_iduser, precio);
+
+        Optional<User> optUser = userRepository.findById(user_iduser);
+
+        if (optUser.isPresent()) {
+            User user = optUser.get();
+            //attr.addAttribute("user_iduser",user.getIduser());
+            //System.out.println(user.getIduser());
+            model.addAttribute("user",user);
+            List listaVuelos = vueloRepository.findAll();
+            model.addAttribute("listaVuelos", listaVuelos);
+            return "homePage";
+
+
+        } else {
+            return "redirect:/";
+        }
+    }
+
 
 }
--- src/main/java/com/example/lab420200334/repository/ReservaRepository.java
+++ src/main/java/com/example/lab420200334/repository/ReservaRepository.java
@@ -3,16 +3,24 @@ package com.example.lab420200334.repository;
 import com.example.lab420200334.entity.Reserva;
 import com.example.lab420200334.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva,Integer> {
 
-    @Query(value = "select * from reserva where user_iduser = ?1 ",
+    @Query(value = "select * from `lab4`.`reserva` where user_iduser = ?1 ",
             nativeQuery = true)
     List<Reserva> encontrarReserva(int id_user);
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into `lab4`.`reserva` (`fecha_reserva`, `precio_total`, `estado_pago`, `vuelo_idvuelo`, `user_iduser`) values ( CURRENT_TIMESTAMP, ?3, 'procesado' , ?1 , ?2) ",
+            nativeQuery = true)
+    void guardarReserva(int vuelo_idvuelo, int user_iduser, double precio_total);
 
 }
